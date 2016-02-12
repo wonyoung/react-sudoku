074d73a097c29f72521c81cda1ffac97966f713b
Wonyoung <wonyoungjjang@gmail.com>
2016-02-12 16:30:46 +0900

    Cursor moves by keyboard input

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ function deepCopy(vs) {
     vs.forEach(v => arr.push(deepCopy(v)));
     return arr;
   }
+  if (typeof vs === "object") {
+    let obj = {};
+    for(let pname in vs) {
+      if (vs.hasOwnProperty(pname)) {
+        obj[pname] = deepCopy(vs[pname]);
+      }
+    }
+    return obj;
+  }
   return vs;
 }
 function dupExists(xs) {
@@ -105,16 +114,31 @@ class Cell extends React.Component {
 
 class InputCell extends React.Component {
   handle(evt) {
-//    console.log(evt.keyCode);
-    const input = parseInt(String.fromCharCode(evt.keyCode));
-    const v = (input > 0 && input < 10) ? input:0;
-    this.props.onInput(this.props.col, this.props.row, v);
+    console.log(evt.keyCode);
+    if (evt.keyCode >= 49 && evt.keyCode <= 58) {
+      this.props.onInput(this.props.col, this.props.row, parseInt(String.fromCharCode(evt.keyCode)));
+    }
+    else if (evt.keyCode === 32) {
+      this.props.onInput(this.props.col, this.props.row, 0);
+    }
+    else if (evt.keyCode >= 37 && evt.keyCode <= 40) {
+      this.props.onMove(evt.keyCode);
+    }
   }
+
+  componentDidMount() {
+    if (this.props.focused) this._input.focus();
+  }
+
+  componentDidUpdate() {
+    if (this.props.focused) this._input.focus();
+  }
+
   render() {
     const classname = this.props.cellClassName;
     return <td className={classname}><input className={classname}
       type="text"
-      ref="cell"
+      ref={c => this._input = c}
       value = {this.props.value}
       onKeyDown={evt => this.handle(evt)}
       /></td>
@@ -128,13 +152,19 @@ class Row extends React.Component {
         {this.props.values.map((n, i) => {
           if (this.props.initValues[i]==0) {
             const value = n > 0 ? n:'';
+            const c = this.props.cursor;
+            const focused = c.x === i && c.y === this.props.row;
+            if (focused) console.log(c);
             return <InputCell
               value={value}
+              focused={focused}
               cellClassName={this.props.cellClassNames[i]}
               key={i}
               row={this.props.row}
               col={i}
-              onInput={this.props.onInput}/>
+              onInput={this.props.onInput}
+              onMove={this.props.onMove}
+              />
           }
           else {
             return <Cell
@@ -157,10 +187,12 @@ class Row3 extends React.Component {
             return <Row
               initValues={this.props.initLines[i]}
               values={line}
+              cursor={this.props.cursor}
               cellClassNames={this.props.cellClassNames[i]}
               key={i}
               row={this.props.row*3+i}
               onInput={this.props.onInput}
+              onMove={this.props.onMove}
               />
           })
         }
@@ -173,8 +205,13 @@ class Sudoku extends React.Component {
   constructor(props) {
     super(props);
     const rows = deepCopy(this.props.init);
+    const inputboxes = rows.map((rs, y) => rs.map((v, x) => {return {x,y,v};})
+                                             .filter(c => c.v === 0))
+                           .reduce((a,b) => a.concat(b));
+    const cursor = inputboxes[0];
     this.state = {
-      rows
+      rows,
+      cursor
     };
   }
 
@@ -182,13 +219,55 @@ class Sudoku extends React.Component {
     let rows = deepCopy(this.state.rows);
     rows[y][x] = v;
     return {
-      rows
+      rows,
+      cursor: this.state.cursor
     };
   }
 
   handleInput(x, y, v) {
     const newState = this.nextState(x, y, v);
     this.setState(newState);
+  }
+
+  handleCursor(keycode) {
+    let cursor = deepCopy(this.state.cursor);
+    if (keycode === 37) {
+      let x = cursor.x;
+      for(let x=cursor.x-1;x>=0;x--) {
+        if (this.props.init[cursor.y][x] === 0) {
+          cursor.x = x;
+          break;
+        }
+      }
+    }
+    if (keycode === 38) {
+      let y = cursor.y;
+      for(let y=cursor.y-1;y>=0;y--) {
+        if (this.props.init[y][cursor.x] === 0) {
+          cursor.y = y;
+          break;
+        }
+      }
+    }
+    if (keycode === 39) {
+      let x = cursor.x;
+      for(let x=cursor.x+1;x<this.props.init.length;x++) {
+        if (this.props.init[cursor.y][x] === 0) {
+          cursor.x = x;
+          break;
+        }
+      }
+    }
+    if (keycode === 40) {
+      let y = cursor.y;
+      for(let y=cursor.y+1;y<this.props.init.length;y++) {
+        if (this.props.init[y][cursor.x] === 0) {
+          cursor.y = y;
+          break;
+        }
+      }
+    }
+    this.setState({rows:this.state.rows, cursor})
   }
 
   render() {
@@ -206,10 +285,12 @@ class Sudoku extends React.Component {
             return <Row3
               initLines={this.props.init.slice(r1, r2)}
               lines={this.state.rows.slice(r1, r2)}
+              cursor={this.state.cursor}
               cellClassNames={cellClassNames.slice(r1, r2)}
               key={i}
               row={i}
               onInput={(x,y,v) => this.handleInput(x,y,v)}
+              onMove={keyCode => this.handleCursor(keyCode)}
               />
           })
         }
